db1dc660589944b98aad5d431726f50b8b2f863d
Jan-Philip Loos <maxdaten@gmail.com>
2019-04-21 03:54:09 +0200

    First faulty nix setup
    ```
    installing
    /nix/store/63karsgdg7fm3q0if4zfd7apbd8ac1ci-stdenv-linux/setup: line 1312: hakyll: command not found
    builder for '/nix/store/23bybqal68yrisnq4y6hfa86lxrx4501-brutal-recipes.drv' failed with exit code 127
    error: build of '/nix/store/23bybqal68yrisnq4y6hfa86lxrx4501-brutal-recipes.drv' failed
    ```

---
+++ default.nix
@@ -0,0 +1,30 @@
+let
+  config = {
+    packageOverrides = nixpkgs: rec {
+      haskellPackages = nixpkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          pandoc = haskellPackagesNew.callPackage ./pandoc.nix { };
+          hakyll = haskellPackagesNew.callPackage ./hakyll.nix { };
+        };
+      };
+    };
+  };
+
+  
+
+  nixpkgs = import <nixpkgs> { inherit config; };
+  inherit (nixpkgs) stdenv;
+  
+  brutalRecipes = stdenv.mkDerivation {
+    name = "brutal-recipes";
+    buildInput = [ nixpkgs.haskellPackages.hakyll ];
+    unpackPhase = "true";
+    buildPhase = "true";
+    installPhase = 
+    ''
+      mkdir -p $out/bin
+      hakyll --version
+      cp ${./index.html} $out/bin/index.html
+    '';
+  };
+in brutalRecipes
