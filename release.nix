c073b1f6739d900aed23dc3811806c8f3dc33571
Jan-Philip Loos <maxdaten@gmail.com>
2019-05-05 00:24:28 +0200

    Pin nixpkgs to 19.03

--- release.nix
+++ release.nix
@@ -1,4 +1,15 @@
-with import <nixpkgs> { };
 let
-    site = callPackage ./default.nix { };
+    bootstrap = import <nixpkgs> { };
+
+    nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+    src = bootstrap.fetchFromGitHub {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        inherit (nixpkgs) rev sha256;
+    };
+
+    pkgs = import src {};
+
+    site = pkgs.callPackage ./default.nix { };
 in site
