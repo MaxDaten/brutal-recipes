0bba6d1c850b88a4b3ea4cd3897dd19efa7f045a
Jan-Philip Loos <maxdaten@gmail.com>
2019-05-31 00:01:14 +0200

    Switch hardcoded to nixpkgs-latest

--- release.nix
+++ release.nix
@@ -1,7 +1,7 @@
 let
     bootstrap = import <nixpkgs> { };
 
-    nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+    nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs-latest.json);
 
     src = bootstrap.fetchFromGitHub {
         owner = "NixOS";
