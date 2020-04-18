f84381296fc41142c1a48a2c516f9338ccc6662b
Jan-Philip Loos <maxdaten@gmail.com>
2020-04-18 23:11:57 +0200

    Adjust site-generator package to conventions
    Following: https://nixos.org/nixpkgs/manual/#how-to-create-nix-builds-for-your-own-private-haskell-packages

--- site-generator/site-generator.nix
+++ site-generator/site-generator.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, hakyll, stdenv }:
+{ mkDerivation, base, hakyll, stdenv, time }:
 mkDerivation {
   pname = "site-generator";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base hakyll ];
+  executableHaskellDepends = [ base hakyll time ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
