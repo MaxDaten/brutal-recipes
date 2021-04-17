081c15679ad0d3e44d65d59ab633c43498a162e2
Jan-Philip Loos <maxdaten@gmail.com>
2021-04-17 13:03:35 +0200

    Adopt nix 2.0 and bump to latest unstable

--- release.nix
+++ release.nix
@@ -1,17 +1,13 @@
 let
-    inherit (import <nixpkgs> {}) fetchFromGitHub;
-
-    # Update via:
-    # `nix-prefetch-git https://github.com/NixOS/nixpkgs-channels --rev refs/heads/master > nixpkgs-latest.json`
-    nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs-latest.json);
-
-    src = fetchFromGitHub {
-        owner = "NixOS";
-        repo = "nixpkgs";
-        inherit (nixpkgs) rev sha256;
-    };
-
-    pkgs = import src {};
+    pkgs = import (builtins.fetchGit {
+        # Descriptive name to make the store path easier to identify
+        name = "nixos-unstable-2021-04-17";
+        url = "https://github.com/nixos/nixpkgs/";
+        # Commit hash for nixos-unstable as of 2020-04-17
+        # `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
+        ref = "refs/heads/nixos-unstable";
+        rev = "dcdf30a78a523296b5f9d44fb67afac485b64737";
+    }) {};
 
     site = pkgs.callPackage ./default.nix { };
 in site
